d8ba081a911130216ba95f53d6ef07a3892eb8ac
jjeswinjacob <jjeswinjacob@gmail.com>
2020-04-22 18:47:13 +0530

    Updates

---
+++ precache-manifest.57c550bb600775281a4c16501582469d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46145a21b3b25772e38b61efdc8c1b52",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b5e28e9d1825f806c8c7",
+    "url": "/monsters-rolodex/static/css/main.6f1fb312.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e28e9d1825f806c8c7",
+    "url": "/monsters-rolodex/static/js/main.74c1be08.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
